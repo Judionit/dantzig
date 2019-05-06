4c88939c5bd0994b896a8b59b8ef2b99840f8c67
Judicael <jmanambintsoa@gmail.com>
2019-05-06 17:48:10 +0300

    adding project

---
+++ algo/dantzig.js
@@ -0,0 +1,22 @@
+function minimisation() {
+   let i = 1;
+   let k = 0;
+   let n = 16;
+   let x; 
+   x > circle.parentNode;
+   let x1; 
+   x1 > cirle.lastChild;
+   let lambda = v(xi,xi);
+   while (i < n) {
+       if(i === 1) {
+           lambda1 = 0;
+       }
+       else {
+           let lambdai = lambdap + v(xp, xp);
+           lambdai = min[lambdai + lambda];
+           lambdai = min[xi + lambda];
+       }
+   }
+   i++;
+   k++;
+}
